8c27ebb4833f6fa43f924426b65788a525ab2a1a
Shazia Anwar <shazi.522122595@gmail.com>
2025-01-17 11:11:26 +0530

    Update index.js
    Problem No 4 Solution  Added

--- 08Chapter2PS/index.js
+++ 08Chapter2PS/index.js
@@ -44,6 +44,21 @@ else {
 }
 */
 
+// Problem No 4
+/*
+let num = prompt("What is your age?")
+num = Number.parseInt(num)
+if (num % 2 == 0) {
+  console.log("Your number is divisible by 2")
+}
+if (num % 3 == 0) {
+  console.log("Your number is divisible by 3")
+}
+else {
+  console.log("Your number is not divisible by 2 and 3")
+}
+*/
+
 // Problem No 5
 let age = 19
 let a = age > 18 ? "You can drive" : "You cannot drive"
